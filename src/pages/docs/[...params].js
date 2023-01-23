d91a63fd5d0bf8654db4520cb83f63abd6a8ddf7
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-01-23 08:02:01 +0530

    [feat]: log catch all routes array

--- src/pages/docs/[...params].js
+++ src/pages/docs/[...params].js
@@ -1,6 +1,12 @@
-import React from 'react';
+import { useRouter } from 'next/router';
 
 const Doc = () => {
+	const router = useRouter();
+	const { params } = router.query;
+	console.log(params);
+	if (params) {
+		return <h1> viewwing {params}</h1>;
+	}
 	return <div>Doc</div>;
 };
 
